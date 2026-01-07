4a557455ce5b1d260c2e8a1061accc3037e191ea
prathamparmar1 <prathamparmar203@gmail.comm>
2026-01-07 23:22:46 +0530

    renamed for proper order

---
+++ read_chunks.py
@@ -0,0 +1,21 @@
+import requests
+import os
+import json
+
+def create_embedding(text):
+    r = requests.post('http://localhost:11434/api/embeddings', json={
+        'model': 'bge-m3',
+        'prompt': text
+    })
+
+    embedding = r.json()['embedding']
+    return embedding
+
+
+jsons = os.listdir('jsons')
+
+for json_file in jsons:
+    with open (f"jsons/{json_file}") as f:
+        content = json.load(f)
+    for chunk in content['chunks']:
+        print(chunk)
