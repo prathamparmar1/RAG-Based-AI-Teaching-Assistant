c115a7d85c3b4672689a87b2b1098539925ea1a9
prathamparmar1 <prathamparmar203@gmail.comm>
2026-01-07 23:23:15 +0530

    successfully generated embeddings of all files

--- read_chunks.py
+++ read_chunks.py
@@ -1,21 +1,37 @@
 import requests
 import os
 import json
+import pandas as pd
 
-def create_embedding(text):
-    r = requests.post('http://localhost:11434/api/embeddings', json={
+def create_embedding(text_list):
+    #https://github.com/ollama/ollama/blob/main/docs/api.md    {multiple input}
+    r = requests.post('http://localhost:11434/api/embed', json={
         'model': 'bge-m3',
-        'prompt': text
+        'input': text_list
     })
 
-    embedding = r.json()['embedding']
+    embedding = r.json()['embeddings']
     return embedding
+
+# embedding = create_embedding("the cat sat on the mat")
+# print(embedding)
 
 
 jsons = os.listdir('jsons')
-
+my_dicts = []
+chunk_id = 0
 for json_file in jsons:
     with open (f"jsons/{json_file}") as f:
         content = json.load(f)
-    for chunk in content['chunks']:
-        print(chunk)
+    print(f"Creating embedding for{json_file}")
+    embeddings = create_embedding([c['text'] for c in content['chunks']])
+    for i, chunk in enumerate(content['chunks']):
+        chunk['chunk_id'] = chunk_id
+        chunk['embedding'] = embeddings[i]
+        chunk_id+=1
+        my_dicts.append(chunk)
+
+# print(my_dicts)
+
+df = pd.DataFrame.from_records(my_dicts)
+print(df)
