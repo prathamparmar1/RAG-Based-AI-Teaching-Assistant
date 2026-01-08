65e24fc6bf8887890b926dc50a6c19218a66da23
prathamparmar1 <prathamparmar203@gmail.comm>
2026-01-08 12:59:29 +0530

    created logic to create file of embeddings of existing chunks using joblib and seperated reading chunks from embedding creation of incoming req and matching.

---
+++ process_incoming.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+import joblib
+import requests
+
+def create_embedding(text_list):
+    #https://github.com/ollama/ollama/blob/main/docs/api.md    {multiple input}
+    r = requests.post('http://localhost:11434/api/embed', json={
+        'model': 'bge-m3',
+        'input': text_list
+    })
+
+    embedding = r.json()['embeddings']
+    return embedding
+
+
+df = joblib.load('embeddings.joblib')
+
+incoming_query = input("Ask a Question: ")
+question_embedding = create_embedding([incoming_query])[0]
+# print(question_embedding)
+
+
+#Find similarities of question_embedding to other embeddings
+similarities = cosine_similarity(np.vstack(df['embedding'].values), [question_embedding]).flatten()
+print(similarities)
+max_similarity_index = similarities.argsort()[::-1][0:3]
+print(max_similarity_index)
+
+new_df = df.loc[max_similarity_index]
+print(new_df[['title', 'number', 'text']])
