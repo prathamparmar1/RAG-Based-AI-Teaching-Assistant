30f2f594cd4af8640910d1e6b7c8a35e8d063abc
prathamparmar1 <prathamparmar203@gmail.comm>
2026-01-08 18:39:05 +0530

    Attached prompt and written to prompt.txt

--- process_incoming.py
+++ process_incoming.py
@@ -25,12 +25,24 @@ question_embedding = create_embedding([incoming_query])[0]
 #Find similarities of question_embedding to other embeddings
 similarities = cosine_similarity(np.vstack(df['embedding'].values), [question_embedding]).flatten()
 # print(similarities)
-top_results = 10
+top_results = 5
 max_similarity_index = similarities.argsort()[::-1][0:top_results]
 # print(max_similarity_index)
 
 new_df = df.loc[max_similarity_index]
 # print(new_df[['title', 'number', 'text']])
 
-for index, item in new_df.iterrows():
-    print(index, item['title'], item['number'], item['text'], item['start'], item['end'])
+prompt  = f'''I am teaching web development sigma web development course. 
+Here are video subtitle chunks containing video title, video number, start time(in seconds), 
+end time(in seconds), the text at that time: "{new_df[['title', 'number', 'start', 'end', 'text']].to_json()}".
+-----------------
+"{incoming_query}"
+User asked this question related to the video chunks, 
+you have to answer where and how much content is taught where(in which video and at what timestamp)
+and guide the user to go to that particular video. If user asks unrelated question
+, tell him that you can only answer question to the course'''
+
+# for index, item in new_df.iterrows():
+#     print(index, item['title'], item['number'], item['text'], item['start'], item['end'])
+with open('prompt.txt', "w") as f:
+    f.write(prompt)
